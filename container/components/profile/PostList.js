3856c93d67024fb99a8b03121e78b094fd9b2dd1
Nghia Tran <trannhannghia.24012000@gmail.com>
2023-03-04 01:05:17 +0700

    add event scroll for profile screen

--- container/components/profile/PostList.js
+++ container/components/profile/PostList.js
@@ -1,5 +1,5 @@
-import { View, Text, StyleSheet, ActivityIndicator, FlatList, TouchableOpacity } from 'react-native'
-import React, { useEffect, useState, useContext } from 'react'
+import { View, Text, StyleSheet, ActivityIndicator, FlatList, TouchableOpacity, ScrollView } from 'react-native'
+import React, { useEffect, useState, useContext, useRef } from 'react'
 import LinearGradient from 'react-native-linear-gradient'
 import colors from '../../assets/colors'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
@@ -8,6 +8,7 @@ import firestore from '@react-native-firebase/firestore';
 import { AuthContext } from '../routes/AuthProvider'
 import Card from '../home/Card'
 import { useNavigation } from '@react-navigation/native';
+import Animated, { Easing } from 'react-native-reanimated'
 
 const PostList = () => {
     const { user, logOut } = useContext(AuthContext)
@@ -15,6 +16,8 @@ const PostList = () => {
     const [loading, setLoading] = useState(true)
 
     const navigation = useNavigation();
+
+    const animatedValue = useRef(new Animated.Value(0)).current;
 
     const fetchPosts = async () => {
         list = [];
@@ -40,23 +43,83 @@ const PostList = () => {
         fetchPosts()
     }, [])
 
+    //Animation
+    const headerBarAnimation_up = {
+        transform: [
+            {
+                translateY: animatedValue.interpolate({
+                    inputRange: [0, 60],
+                    outputRange: [0, -60],
+                    extrapolate: 'clamp'
+                })
+            },
+        ],
+    }
+
+    const headerBarAnimation_down = {
+        transform: [
+            {
+                translateY: animatedValue.interpolate({
+                    inputRange: [0, 60],
+                    outputRange: [-60, 0],
+                    extrapolate: 'clamp'
+                })
+            },
+        ],
+    }
+
+    const iconAnimation = {
+        transform: [
+            {
+                translateX: animatedValue.interpolate({
+                    inputRange: [0, 60],
+                    outputRange: [0, -300],
+                    extrapolate: 'clamp'
+                })
+            },
+        ],
+    }
+
+    const textAnimation = {
+        transform: [
+            {
+                translateX: animatedValue.interpolate({
+                    inputRange: [0, 60],
+                    outputRange: [0, 300],
+                    extrapolate: 'clamp'
+                })
+            },
+        ],
+    }
+
     return (
         <LinearGradient colors={[`${colors.secondColor}`, `${colors.thirdColor}`]} style={styles.container}>
-            <View style={styles.header}>
+            <Animated.View style={[styles.header, headerBarAnimation_up]}>
                 <TouchableOpacity onPress={() => { navigation.goBack() }}>
-                    <View style={styles.backgroundIcon} >
+                    <Animated.View style={[styles.backgroundIcon, iconAnimation]} >
                         <FontAwesomeIcon icon={faArrowCircleLeft} size={20} color={colors.primaryColor} />
-                    </View>
+                    </Animated.View>
                 </TouchableOpacity>
-                <Text style={styles.heading}>Posts</Text>
-            </View>
+                <Animated.Text style={[styles.heading, textAnimation]}>Posts</Animated.Text>
+            </Animated.View>
             <View style={styles.body}>
-                {loading ? <ActivityIndicator size='large' color={colors.primaryColor} animating /> : <FlatList
-                    data={dataPosting}
-                    renderItem={({ item }) => <Card items={item} />}
-                    keyExtractor={item => item.id}
-                    showsVerticalScrollIndicator={false}
-                />}
+                {loading ? <ActivityIndicator size='large' color={colors.primaryColor} animating /> :
+                    <ScrollView
+                        showsVerticalScrollIndicator={false}
+                        onScroll={e => {
+                            const offsetY = e.nativeEvent.contentOffset.y;
+                            animatedValue.setValue(offsetY)
+                        }}
+                        scrollEventThrottle={16}
+                    >
+                        {dataPosting.map((value, index) => (
+                            <View key={index}>
+                                <View style={{ marginTop: 60 }} />
+                                <Card items={value} />
+                            </View>
+                        ))}
+                    </ScrollView>
+                }
             </View>
         </LinearGradient>
     )
@@ -67,7 +130,10 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     header: {
-        flex: 1,
+        position: 'absolute',
+        height: 60,
+        width: '100%',
+        // flex: 1,
         backgroundColor: colors.secondColor,
         flexDirection: 'row',
         justifyContent: 'space-between',
@@ -78,7 +144,7 @@ const styles = StyleSheet.create({
         // shadowOpacity: 0.5,
         // shadowRadius: 2,
         elevation: 1, // changed to a greater value
-        // zIndex: 99, // added zIndex
+        zIndex: 99, // added zIndex
     },
     heading: {
         color: colors.headingColor,
